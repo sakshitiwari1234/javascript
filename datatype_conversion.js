5e648b7201dcf6748a7afb1949552fc31471b11a
sakshitiwari1234 <134698162+sakshitiwari1234@users.noreply.github.com>
2024-04-25 12:23:01 +0000

    datatype_conversion

---
+++ datatype_conversion.js
@@ -0,0 +1,27 @@
+let score  = 43
+
+console.log(typeof score);   // can also seen as passing as method into parenthesis or as this also
+console.log("sakshi");
+
+let valuenumber = Number(score)
+console.log(typeof valuenumber);
+
+// console.log("2324");
+let a = "435"
+console.log(typeof a);
+
+let value = Number(a)
+console.log(typeof value);
+
+console.log(value );     //here the string which had number stored got converted into number tyype and showing the correct value
+
+// lets check does actual string get convert or not and if yes then what is the value number type shows.
+
+let b = "thi is string "
+console.log(typeof b);
+let thesis = Number(b)
+console.log(typeof thesis);
+console.log(thesis);
+
+
+
