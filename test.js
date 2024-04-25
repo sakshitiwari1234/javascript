51d06296e06b7ea13ed5669b1d5a5886de436e47
sakshitiwari1234 <134698162+sakshitiwari1234@users.noreply.github.com>
2024-04-25 11:32:08 +0000

    datatypes

--- test.js
+++ test.js
@@ -15,6 +15,25 @@ console.log('saksshi tiwari here and i m learning jvascript')
 
 // ECMA standars ha all the standar rule and regulations for using the jaavscript language.*tc39*
 
+let name  = "sakshi"
+let age = 20
+let isloggedin = false
+
+
+// some special datatype are
+// number  = 2 to the power 53
+// bigint 
+// string = ""
+// boolean = 'true of false either
+// null -  null is a datatype but most of all it is a standalone value.
+// undefined = undefined means that the datatype is made but holds no value, on running that the terminal would output no.
+
+// symbol = mostly used in react to find the uniqueness among same component in designs and react we use ssymbol. all discusses till here are primitive datatypesss. 
+
+
+console.log(typeof null);    // null has null type
+console.log(typeof undefined);   // undefnd has undefined type
+console.log(typeof "sakshi");
 
 
 
