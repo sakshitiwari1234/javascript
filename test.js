1208ee636c068164c1901a5585cfb91674708978
sakshitiwari1234 <134698162+sakshitiwari1234@users.noreply.github.com>
2024-04-25 09:14:36 +0000

    Datatypes and ECMA standards

---
+++ test.js
@@ -0,0 +1,20 @@
+// html and javascript 
+// alert(3 + 3)    // we are using node js not browser by default.
+// in react and node mosstly the semicolns are ignored only 
+
+// redibility matter most in javascript 
+
+console.log(32+45456)
+console.log(435+45234)
+
+console.log('saksshi tiwari here and i m learning jvascript')
+
+
+//  documnetation 
+//refer to mdn website for the documentation of all standards and learn more about usage of javascrpt programming language
+
+// ECMA standars ha all the standar rule and regulations for using the jaavscript language.*tc39*
+
+
+
+
