41f9c00b5ab348d7ba68b82c5bf7b53acb3a084a
sakshitiwari1234 <134698162+sakshitiwari1234@users.noreply.github.com>
2024-04-28 09:29:34 +0000

    datatype_Concersion_2.js done

---
+++ string_to_number.js
@@ -0,0 +1,31 @@
+/////operations in javascript.
+
+console.log(2*2)
+let value = 3;
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2-2);
+// same operations can be performed for all arithmatic operations 
+
+let str1 = 'hello'
+let str2 = 'sakshi tiwari here'
+let str3 = str1 + str2
+console.log(str3);
+console.log(3+4*5%3);
+/////////////////////////////////
+console.log("1" +2+2);
+console.log(1+2+"2");
+
+///////////////////////////////
+
+// lecture 8 from here onwards , comparision of datatypes
+
+console.log(2 > 1);
+console.log(2 == 2);
+console.log("02" > 1);    // must ensure that the datatype we are using to compare the dataype must be of same type.
+
+
+console.log(null > 0);
+console.log(null >= 0);
+
